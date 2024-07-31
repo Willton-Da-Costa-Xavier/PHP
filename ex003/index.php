26c1ffc261e23004e92aeb53cf623a5540322cd7
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-07-31 19:41:57 +0200

    Terminei a minha sessao da aula

---
+++ ex003/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <h1>Testes de tipos Primitivos</h1>
+    <?php 
+    //    $num = 010;
+    //    echo "O valor da variavel e: ". $num;
+
+    // $v ="Willton";
+    // var_dump($v);
+
+    // $num = (float) "450";
+    // var_dump($num);
+
+    $casado = false;
+    var_dump($casado);
+    
+
+    ?>
+</body>
+</html>

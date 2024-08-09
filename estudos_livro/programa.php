b4281f6c06530f6419707607a5c0059e1c7f40ee
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-08-09 21:20:06 +0200

    Atualizei o programa.php

---
+++ estudos_livro/programa.php
@@ -0,0 +1,10 @@
+<?php 
+echo "Hoje e dia ". date("l");
+echo " agora sao ". date("h a");
+//y - ano com 2 digitos
+//h - hora com 2 digitos
+//a - am e pm em minusculo
+//l - dia da semana completo
+//D - dia da semana em 3 letras
+
+?>

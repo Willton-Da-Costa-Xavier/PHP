9a18dce4aa0c1e7f2b9a25f7074dd6c506a8ecd5
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-08-10 17:38:30 +0200

    criei o calendario

---
+++ estudos_livro/calendario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?="Titulo dentro do H1"?></h1>
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sab</th>
+        </tr>
+    </table>
+</body>
+</html>

b8c27be54cc2a5519d92f774c62279ebf782941a
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-08-10 17:28:44 +0200

    usando a funcao date no php

--- estudos_livro/programa.php
+++ estudos_livro/programa.php
@@ -1,10 +1,21 @@
 <?php 
-echo "Hoje e dia ". date("l");
-echo " agora sao ". date("h a");
+// Obter o dia atual da semana (1 = segunda-feira, 7 = domingo)
+// $diaAtual = date('N');
+
+// Calcular os dias faltantes para o próximo sábado
+// $diasParaSabado = (6 - $diaAtual + 7) % 7;
+
+// Se o resultado for 0, significa que hoje é sábado, então adiciona 7 dias
+// $diasParaSabado = $diasParaSabado ?: 7;
+
+// echo "Faltam $diasParaSabado dias para o próximo sábado.";
+echo "O mes atual e ". date("F");
+
 //y - ano com 2 digitos
 //h - hora com 2 digitos
 //a - am e pm em minusculo
 //l - dia da semana completo
 //D - dia da semana em 3 letras
+//F - mes completo
 
 ?>

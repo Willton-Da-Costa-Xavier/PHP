2c2df7ddf0843321e4471741b0ee2cfd6268826c
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-08-10 17:43:50 +0200

    Criei o hoje.php

---
+++ estudos_livro/hoje.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=date('d')?></title>
+</head>
+<body>
+    <h1>Estamos em <?=date("Y")?></h1>
+    <p>Agora sao <?=date("H")?> horas e <?=date("i")?> minutos.</p>
+</body>
+</html>
